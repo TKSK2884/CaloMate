602a9e3013118ec35cafa6d79843d63597fb1ce0
Rufree <tksk2884@gmail.com>
2024-12-04 18:20:26 +0900

    테마 토글 컴포넌트 추가

---
+++ components/ThemeToggle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <UButton
+            v-if="colorMode.preference == ThemeColor.Dark"
+            icon="i-heroicons-sun"
+            color="gray"
+            variant="ghost"
+            @click="getPreference(ThemeColor.Light)"
+        />
+        <UButton
+            v-if="colorMode.preference == ThemeColor.Light"
+            icon="i-heroicons-moon"
+            color="gray"
+            variant="ghost"
+            @click="getPreference(ThemeColor.Dark)"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ThemeColor } from "~/structure/theme";
+
+const colorMode = useColorMode();
+
+const getPreference = (color: ThemeColor) => {
+    colorMode.preference = color;
+};
+</script>
